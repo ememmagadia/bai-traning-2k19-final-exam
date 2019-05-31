702be6f0ca8aacbb5c5670aa93ccdd7882d2175c
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-05-31 15:27:20 +0800

    Exam 5-31-19 Final Commit

---
+++ Anagrams/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<string, string>() {
+                {"heater","reheat"},
+                { "Arc","car"} ,
+                { "Elbow","below"} ,
+                { "NIght","thing"} ,
+                { "cshark","csharp"} ,
+                { "Dormitory","Dirty room"} ,
+                { "School master","The classroom"} ,
+                { "php","hpp,." },
+                {"Astronomer ", "Moon starer" }
+
+            };
+
+            string x = "reheat";
+            string y = "heater";
+
+            string newX = x.ToLower(); newX = newX.Replace(" ", "");
+            string newY = y.ToLower(); newY = newY.Replace(" ", "");
+
+            List<char> listy1 = new List<char>();
+            List<char> listy2 = new List<char>();
+
+            foreach (var i in newX)
+            {
+                listy1.Add(i);
+                listy1.Sort();
+            }
+            foreach (var i in newY)
+            {
+                listy2.Add(i);
+                listy2.Sort();
+            }
+            
+
+        }
+    }
+}
