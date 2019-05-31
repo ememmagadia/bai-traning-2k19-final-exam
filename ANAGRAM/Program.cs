06b303b46956b1ac9efaf78f6fc176ab9f02ef23
orilla-jasper <jasper.orilla915@gmail.com>
2019-05-31 15:33:22 +0800

    Added Anagram

---
+++ ANAGRAM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ANAGRAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<string, string>() {
+                {"heater","reheat"},
+                { "Arc","car"} ,
+                { "Elbow","below"} ,
+                { "NIght","thing"} ,
+                { "cshark","csharp"} ,
+                { "Dormitory","Dirty room"} ,
+                { "School master","The classroom"} ,
+                { "php","hpp,." },
+                {"Astronomer ", "Moon starer" }
+                
+            };
+            String x;
+            foreach (var i in words)
+            {
+                if (i.Key.Contains(i.Value))
+                {
+                    x = "Anagram";
+                }
+                else
+                {
+                    x = "Not Anagram";
+                }
+                Console.WriteLine(i + x);
+            }
+            
+
+
+        }
+    }
+}
