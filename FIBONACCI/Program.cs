0670fa63f172bb6a15feedc05c699f16ea9ca151
orilla-jasper <jasper.orilla915@gmail.com>
2019-05-31 15:01:09 +0800

    updated ex 2

--- FIBONACCI/Program.cs
+++ FIBONACCI/Program.cs
@@ -8,17 +8,18 @@ namespace FIBONACCI
         {
             Console.WriteLine("Please Enter a number");
             int a = int.Parse(Console.ReadLine());
-            int[] array ={ 0, 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+            int[] array ={ 0, 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
             
             for (int i = 2; i < a; i++)
             {
                 array[i] = array[i - 1] + array[i - 2];
             }
+
                 for (int i = 0; i < a; i++)
                 {
                     Console.Write(array[i] + " ");
                 }
-               
+
 
         }
     }
