a4e15ccfd84adb45aa25713684a673f00d2be249
Kyla C <kyla.calpito@gmail.com>
2019-05-31 15:11:23 +0800

    Mini Information System. Delete not working.

---
+++ MiniInformationSystem/Person.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniInformationSystem
+{
+    class Person
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Person> PopulatePerson()
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person()
+                {
+                    FirstName = "Kyla",
+                    LastName = "Calpito",
+                    UserId = 0
+                },
+                new Person()
+                {
+                    FirstName = "Jelmarose",
+                    LastName = "De Vera",
+                    UserId = 1
+                },
+                new Person()
+                {
+                    FirstName = "Marc",
+                    LastName = "Lomio",
+                    UserId = 2
+                },
+                new Person()
+                {
+                    FirstName = "Lex",
+                    LastName = "Carao",
+                    UserId = 3
+                },
+                new Person()
+                {
+                    FirstName = "Dino",
+                    LastName = "Reyes",
+                    UserId = 4
+                },
+                new Person()
+                {
+                    FirstName = "Charles",
+                    LastName = "Nazareno",
+                    UserId = 5
+                },
+                new Person()
+                {
+                    FirstName = "Arnold",
+                    LastName = "Mendoza",
+                    UserId = 6
+                },
+                new Person()
+                {
+                    FirstName = "Ellah",
+                    LastName = "Agni",
+                    UserId = 7
+                },
+                new Person()
+                {
+                    FirstName = "Melrose",
+                    LastName = "Mejidana",
+                    UserId = 8
+                },
+                new Person()
+                {
+                    FirstName = "Jasper",
+                    LastName = "Orilla",
+                    UserId = 9
+                },
+                new Person()
+                {
+                    FirstName = "Aaron",
+                    LastName = "Custodio",
+                    UserId = 10
+                }
+            };
+
+            return people;
+        }
+        public List<Person> AddPerson(List<Person> people, string firstName, string lastName)
+        {
+            people.Add(
+                    new Person
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        UserId = people.Count()
+                    }
+                );
+
+            Console.WriteLine("Successfully added record!");
+            return people;
+        }
+
+        public List<Person> DeletePerson(List<Person> people,string keyword)
+        {
+            var records = from person in people
+                          where String.Equals(person.LastName, keyword, StringComparison.CurrentCultureIgnoreCase)
+                          select new
+                          {
+                              FirstName = person.FirstName,
+                              LastName = person.LastName,
+                              UserId = person.UserId
+                          };
+
+            if(records.Count() > 0)
+            {
+                foreach (var item in records)
+                {
+                    Person deletePerson = new Person()
+                    {
+                        FirstName = item.FirstName,
+                        LastName = item.LastName,
+                        UserId = item.UserId
+                    };
+
+                    int index = people.IndexOf(deletePerson);
+                    if(index > 0)
+                    {
+                        people.RemoveAt(index);
+                        Console.WriteLine("Successfully deleted {0}.", keyword);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No last name found for {0}.", keyword);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No last name found for {0}.", keyword);
+            }
+            return people;
+        }
+
+        public List<Person> ViewPerson(List<Person> people, string keyword)
+        {
+            var records = from person in people
+                          where String.Equals(person.LastName, keyword, StringComparison.CurrentCultureIgnoreCase)
+                          select new
+                          {
+                              FirstName = person.FirstName,
+                              LastName = person.LastName,
+                              UserId = person.UserId
+                          };
+
+            if(records.Count() > 0)
+            {
+                return people;
+            }
+            else
+            {
+                Console.WriteLine("No records found for {0}.", keyword);
+                return people;
+            }
+        }
+    }
+}
---
+++ MiniInformationSystem/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniInformationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            List<Person> people = new List<Person>();
+            people = person.PopulatePerson();
+
+            while(true)
+            {
+                Console.Write("ADD, DELETE OR VIEW? ");
+                string command = Console.ReadLine().ToUpper();
+
+                if (command == "ADD")
+                {
+                    Console.Write("Enter First Name: ");
+                    string firstName = Console.ReadLine();
+
+                    Console.Write("Enter Last Name: ");
+                    string lastName = Console.ReadLine();
+
+                    // Update people list
+                    people = person.AddPerson(people, firstName, lastName);
+                }
+                else if (command == "DELETE")
+                {
+                    // Do something
+                    Console.Write("Enter Last Name of record to be deleted: ");
+                    string lastName = Console.ReadLine();
+
+                    // Delete from people list
+                    people = person.DeletePerson(people, lastName); ;
+                }
+                else if (command == "VIEW")
+                {
+                    Console.WriteLine("Type ALL to view all records./n Type a person's last name to view a specific record.");
+                    string specific = Console.ReadLine().ToUpper();
+                    if (specific == "ALL")
+                    {
+                        PrintArray(people);
+                    }
+                    else
+                    {
+                        var records = person.ViewPerson(people, specific);
+                        PrintArray(records);
+                    }
+                }
+                else
+                {
+                    Console.Write("Please enter ADD/DELETE/VIEW.");
+                }
+            }
+            
+        }
+
+        static void PrintArray(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine("Record {0}: {1} {2}", person.UserId, person.FirstName, person.LastName);
+            }
+        }
+    }
+}
