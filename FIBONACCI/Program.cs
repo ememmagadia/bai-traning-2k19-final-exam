cee85558929fed1c5b565eaa132eb48e9f422b9f
orilla-jasper <jasper.orilla915@gmail.com>
2019-05-31 14:59:34 +0800

    Added Fibonacci

---
+++ FIBONACCI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FIBONACCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a number");
+            int a = int.Parse(Console.ReadLine());
+            int[] array ={ 0, 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+            
+            for (int i = 2; i < a; i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+                for (int i = 0; i < a; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+               
+
+        }
+    }
+}
