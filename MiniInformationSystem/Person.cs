6c9f1db1b10336760f2fc35e7d1c61cb0a015b0b
BlastAsia Cloud02 <baicloud02@outlook.com>
2019-05-31 14:35:33 +0800

    Person.view done Person.add done

---
+++ MiniInformationSystem/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Linq;
+
+namespace MiniInformationSystem
+{
+	public class Person
+	{
+		public int ID { get; set; }
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public Person (int id=0, string fname="No Name", string lname= "No Last")
+		{
+			ID = id;
+			FirstName = fname;
+			LastName = lname;
+		}
+
+		public ArrayList Add(ArrayList personlist, int id, string fname, string lname)
+		{
+			personlist.Add(
+				new Person{
+					ID = id, FirstName =  fname, LastName = lname });
+			Console.WriteLine("Added");
+			Console.ReadKey();
+			return personlist;
+
+		}
+
+		public ArrayList Delete(ArrayList personlist, int id)
+		{
+			personlist.Remove(id);
+
+			Console.Write("Removed");
+			Console.ReadKey();
+
+			return personlist;
+		}
+
+		public void View(ArrayList personlist)
+		{
+			var personlistenum = personlist.ToArray();
+			var query = from person in personlistenum select person;
+			foreach (Person s in query)
+			{
+				Console.WriteLine("ID : {0}\nFirsname : {1}\nLastname : {2}\n", s.ID, s.FirstName, s.LastName);
+			}
+			Console.ReadKey();
+
+		}
+
+		public void ViewPerson(ArrayList personlist, string lname)
+		{ 
+			var personlistenum = personlist.ToArray();
+			var query = from person in personlistenum where LastName == lname select person;
+			foreach (Person s in query)
+			{
+				Console.WriteLine("ID : {0}\nFirsname : {1}\nLastname : {2}\n", s.ID, s.FirstName, s.LastName);
+			}
+			Console.ReadKey();
+		}
+	}
+}
+
---
+++ MiniInformationSystem/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MiniInformationSystem;
+using System.Collections;
+
+namespace MiniInformationSystem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new Person();
+			ArrayList personlist = new ArrayList()
+			{
+				new Person()
+				{
+					ID = 1,
+					FirstName = "Mermellah",
+					LastName = "Angni"
+				},
+				new Person()
+				{
+					ID = 2,
+					FirstName = "Aaron",
+					LastName = "Custodio"
+				},
+				new Person()
+				{
+					ID = 3,
+					FirstName = "Arnold",
+					LastName = "Mendoza"
+				}
+			};
+
+			Console.Write("Mini Information System\nA\t Add Person\nB\t Delete Person\nC\t View All\nD\t View Person\n\nChoice :");
+			var choice = Console.ReadLine();
+
+			switch(choice)
+			{
+				case "A":
+					Console.Write("Enter ID : ");
+					int id = Convert.ToInt32(Console.ReadLine());
+					Console.Write("Enter First Name : ");
+					string fname = Console.ReadLine();
+					Console.Write("Enter Last Name : ");
+					string lname = Console.ReadLine();
+					personlist = person.Add(personlist, id, fname, lname);
+					break;
+				case "B":
+					Console.Write("Enter ID to Delete : ");
+					int idDelete = Convert.ToInt32(Console.ReadLine());
+					personlist = person.Delete(personlist, idDelete);
+					break;
+				case "C":
+					person.View(personlist);
+					break;
+				case "D":
+					Console.Write("Enter Last Name : ");
+					string lastname = Console.ReadLine();
+					person.ViewPerson(personlist,lastname);
+					break;
+			}
+
+		}
+
+	}
+}
