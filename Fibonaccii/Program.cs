895eab336e235a5eee3691e719728a888392774f
jelmarose <jelmarosedevera@gmail.com>
2019-05-31 16:24:01 +0800

    Exam5-31-19-FinaCommit

---
+++ Fibonaccii/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fibonaccii
+{
+    class Program
+    {
+
+        public int PrintFibonacci(int currentNo, int lastNo)
+        {
+            if (currentNo == 0 || currentNo == 1)
+            {
+                currentNo++;
+                return currentNo;
+            }
+            else
+            {
+                currentNo += lastNo;
+            }
+            return currentNo;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            int currentNo = 0;
+            int lastNo;
+            while (num > 0)
+            {
+                lastNo = currentNo;
+                currentNo = p.PrintFibonacci(currentNo, lastNo);
+                Console.Write(currentNo + " ");
+                //Console.WriteLine("Current: {0} Last: {1}", currentNo, lastNo);
+                num--;
+            }
+        }
+    }
+}
