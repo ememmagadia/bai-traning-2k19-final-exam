c0d1d195126258abbd080448c218ee7df78755a5
Kyla C <kyla.calpito@gmail.com>
2019-05-31 15:01:19 +0800

    Fibonacci sequence

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int max = int.Parse(Console.ReadLine());
+            fibonacci(0, 1, max, 0);
+        }
+
+        static void fibonacci(int firstNum, int secondNum, int max, int cycle)
+        {
+            int temp = 0;
+
+            cycle++;
+            Console.Write("{0} ", firstNum);
+            temp = firstNum;
+            firstNum = secondNum;
+            secondNum = firstNum + temp;
+
+            if (cycle != max)
+            {
+                fibonacci(firstNum, secondNum, max, cycle);
+            }
+        }
+    }
+}
