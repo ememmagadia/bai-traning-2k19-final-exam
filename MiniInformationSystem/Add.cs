28240856b9a087b4dd7bdd8789c35eac1905bd02
jelmarose <jelmarosedevera@gmail.com>
2019-05-31 14:18:03 +0800

    Exercise 1 done

---
+++ MiniInformationSystem/Add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniInformationSystem
+{
+    public class AddPerson
+    {
+        public List<Person> Add (List<Person> personList, Person person)
+        {
+            personList.Add(person);
+            return personList;
+        }
+    }
+}
---
+++ MiniInformationSystem/Delete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniInformationSystem
+{
+    public class DeletePerson
+    {
+        public List<Person> Delete(List<Person> personList, Person person)
+        {
+            personList.Remove(person);
+            return personList;
+        }
+    }
+}
